282dd40ef8680e3b32176e39eb3273773b29b898
Renan Rodrigues Ramos <renanrramossi@gmail.com>
2019-08-22 20:27:32 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+const productsRoutes = require('./api/routes/products');
+const ordersRoutes = require('./api/routes/orders');
+
+app.use(morgan('dev'))
+
+app.use('/products', productsRoutes);
+app.use('/orders', ordersRoutes);
+
+module.exports = app;
