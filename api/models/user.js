4197920ecaccc410c4b7e7260276cc58d46e1a4a
Renan Rodrigues Ramos <renanrramossi@gmail.com>
2019-09-04 20:38:34 -0300

    Adicionada validação de email

--- api/models/user.js
+++ api/models/user.js
@@ -6,7 +6,7 @@ const userSchema = mongoose.Schema({
         type: String, 
         required: true, 
         unique: true, 
-        match:  },
+        match:  /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/},
     password: { type: String, required: true }
 });
 
