369d2d4171af21dd74b5ff86038cc28af6a48e35
Renan Rodrigues Ramos <renanrramossi@gmail.com>
2019-09-04 20:36:22 -0300

    Adicionada validação de email

--- api/models/user.js
+++ api/models/user.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    email: { type: String, required: true, unique: true },
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true, 
+        match:  },
     password: { type: String, required: true }
 });
 
